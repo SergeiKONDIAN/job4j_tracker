1e5a2417a0b0fa0e0974782bd2a74c643bf649c5
Sergei <kondjansereg@mail.ru>
2022-01-05 16:18:59 +0700

    3. Перегрузить метод max для трех чисел [#122644]

---
+++ src/main/java/ru/job4j/oop/Max.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+public class Max {
+    public static int max(int left, int right) {
+        return left > right ? left : right;
+    }
+
+    public static int max(int left, int middle, int right) {
+        return left > right ? max(left, middle) : max(right, middle);
+    }
+}
---
+++ src/test/java/ru/job4j/oop/MaxTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MaxTest {
+
+    @Test
+    public void max20() {
+        int rls = Max.max(10, 20, -50);
+        assertEquals(rls, 20);
+    }
+
+    @Test
+    public void max30() {
+        int rls = Max.max(-30, 30, 30);
+        assertEquals(rls, 30);
+    }
+}
