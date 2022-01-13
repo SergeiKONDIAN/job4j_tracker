cc101ba1df053dc689666b95faa81b1eaceb70ba
Sergei <kondjansereg@mail.ru>
2022-01-13 20:29:27 +0700

    1. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/main/java/ru/job4j/search/Person.java
@@ -0,0 +1,38 @@
+package ru.job4j.search;
+
+public class Person {
+    private String name;
+    private String surname;
+    private String phone;
+    private String address;
+
+    public Person(String name, String surname, String phone, String address) {
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public boolean contains(String key) {
+        return this.name.contains(key)
+                || this.surname.contains(key)
+                || this.phone.contains(key)
+                || this.address.contains(key);
+    }
+}
