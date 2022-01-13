b362cd18f7e3eb560940741965b87d567a693b9b
Sergei <kondjansereg@mail.ru>
2022-01-13 20:30:22 +0700

    0. Коллекции, ArrayList, List, Обобщения [#10094 #250005]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 public class UsageArrayList {
 
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<String> list = new ArrayList<>();
         list.add("Petr");
         list.add("Ivan");
         list.add("Stepan");
-        for (Object value : list) {
+        for (String value : list) {
             System.out.println(value);
         }
     }
