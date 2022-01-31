1f9134d5054f43edee43a93c995b766acbf5b4d3
Sergei <kondjansereg@mail.ru>
2022-01-31 23:22:54 +0700

    1.1. Тесты на StartUI. [#223313 #258453]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -217,7 +217,7 @@ public class StartUITest {
     public void whenInvalidExit() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] { "13", "0" }
+                new String[] {"13", "0"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = new UserAction[]{
