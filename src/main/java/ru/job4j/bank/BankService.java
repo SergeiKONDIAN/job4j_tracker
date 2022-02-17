5b60012d71e00558ce21f18206a7706fa130adff
Sergei <kondjansereg@mail.ru>
2022-02-17 20:41:21 +0700

    3. Банковские переводы [#10038]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,60 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<Account>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        List<Account> acList = users.get(findByPassport(passport));
+        if (!acList.contains(account)) {
+            acList.add(account);
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> acList = users.get(user);
+            for (Account account : acList) {
+                if (account.getRequisite().equals(requisite)) {
+                    return account;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        Account destAccount = findByRequisite(destPassport, destRequisite);
+        if (srcAccount == null || destAccount == null || srcAccount.getBalance() < amount) {
+            return rsl;
+        } else {
+            double balanceSrcNew =  srcAccount.getBalance() - amount;
+            double balanceDestNew =  destAccount.getBalance() + amount;
+            srcAccount.setBalance(balanceSrcNew);
+            destAccount.setBalance(balanceDestNew);
+            rsl = true;
+        }
+        return rsl;
+    }
+}
