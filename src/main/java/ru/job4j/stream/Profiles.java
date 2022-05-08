e431d4730bd0129c09768d54ca77af24e54e5f98
Sergei <kondjansereg@mail.ru>
2022-05-08 16:42:13 +0700

    2. Список адресов [#110062 #292538]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,22 +1,21 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
-        List<Address> rsl = profiles.stream()
+        return profiles.stream()
                 .map(Profile::getAddress)
                 .collect(Collectors.toList());
-        return rsl;
     }
 
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
-                .sorted((address1, address2) -> address1.getCity().compareTo(address2.getCity()))
+                .sorted(Comparator.comparing(Address::getCity))
                 .distinct()
                 .collect(Collectors.toList());
     }
